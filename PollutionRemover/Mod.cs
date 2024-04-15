ebec82e2fb24cc92719dd15fad222fd70fe7fe20
Bruceyboy24804 <aaronbruce100@gmail.com>
2024-04-15 19:44:29 +0100

    started working on reset pollution button

--- PollutionRemover/Mod.cs
+++ PollutionRemover/Mod.cs
@@ -6,20 +6,37 @@ using Game.SceneFlow;
 using Game.Simulation;
 using UnityEngine.PlayerLoop;
 using Unity.Entities;
+using Colossal.IO.AssetDatabase;
+
 
 namespace NoPollution
 {
     public class Mod : IMod
     {
+         internal ModSettings ActiveSettings { get; set; }
+       
         public static ILog log = LogManager.GetLogger($"{nameof(NoPollution)}.{nameof(Mod)}").SetShowsErrorsInUI(false);
-
 
         public void OnLoad(UpdateSystem updateSystem)
         {
+
+            ModSettings activeSettings = new(this);
+            activeSettings.RegisterInOptionsUI();
+
+            Localization.LoadTranslations(activeSettings, log);
+
+
+            AssetDatabase.global.LoadSettings("NoPollution", ActiveSettings, new ModSettings(this));
+
+
             log.Info(nameof(OnLoad));
 
             if (GameManager.instance.modManager.TryGetExecutableAsset(this, out var asset))
                 log.Info($"Current mod asset at {asset.path}");
+
+
+
+            
 
             updateSystem.World.GetOrCreateSystemManaged<NoisePollutionSystem>().Enabled = false;
             updateSystem.World.GetOrCreateSystemManaged<NetPollutionSystem>().Enabled = false;
@@ -27,22 +44,12 @@ namespace NoPollution
             updateSystem.World.GetOrCreateSystemManaged<GroundWaterPollutionSystem>().Enabled = false;
             updateSystem.World.GetOrCreateSystemManaged<WaterPipePollutionSystem>().Enabled = false;
             updateSystem.World.GetOrCreateSystemManaged<AirPollutionSystem>().Enabled = false;
-            
-
-
-
-
-
-
-
         }
-
-
-
 
         public void OnDispose()
         {
             log.Info(nameof(OnDispose));
         }
+
     }
 }
---
+++ PollutionRemover/ModSettings.cs
@@ -0,0 +1,47 @@
+﻿using Colossal;
+using Colossal.IO.AssetDatabase;
+using Game;
+using Game.Modding;
+using Game.Settings;
+using Game.UI;
+using Game.UI.Widgets;
+using JetBrains.Annotations;
+using NoPollution;
+using System.Collections.Generic;
+using Unity.Entities;
+
+namespace NoPollution
+{
+    [FileLocation(nameof(NoPollution))]
+
+    public class ModSettings : ModSetting
+    {
+        
+        public ModSettings(IMod mod) : base(mod)
+        {
+        }
+
+        [SettingsUIButton]
+        public bool ResetPollution
+        {
+            set
+            {
+                // Log a message here using your mod's logger.
+            }
+        }
+
+
+
+
+
+
+        public override void SetDefaults()
+        {
+
+        }
+        public void Unload()
+        {
+
+        }
+    }
+}
