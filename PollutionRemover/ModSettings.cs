d6954feaa962a66e440d46d263fc678d8ce62c9e
Bruceyboy24804 <aaronbruce100@gmail.com>
2024-04-22 11:30:37 +0100

    changed code

--- PollutionRemover/ModSettings.cs
+++ PollutionRemover/ModSettings.cs
@@ -58,7 +58,7 @@ namespace NoPollution
             {
                 _noisePollutionSystem = value;
 
-                Mod.ActiveWorld.GetOrCreateSystemManaged<NoisePollutionSystem>().Enabled = ;
+                Mod.ActiveWorld.GetOrCreateSystemManaged<NoisePollutionSystem>().Enabled = value;
             }
         }
 
